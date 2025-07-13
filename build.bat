75e4e4faa8d298796622bd460abf2fb7baa7ca64
adriele-mesquita <140767548+adriele-mesquita@users.noreply.github.com>
2025-07-13 14:52:34 -0300

    agora vai

--- build.bat
+++ build.bat
@@ -2,10 +2,20 @@
 REM Script de build automático para Jenkins (Windows)
 echo Iniciando processo de build do projeto Django Food Travel...
 
-REM 1. Navegar para o diretório do projeto 
+REM 1. Navegar para o diretório do projeto (
 REM CD %WORKSPACE%
 
-REM 2. Ativar o ambiente virtual
+REM 2. Criar e Ativar o ambiente virtual
+echo Verificando e criando ambiente virtual se necessario...
+IF NOT EXIST venv\ (
+    echo Criando ambiente virtual...
+    python -m venv venv
+    IF %ERRORLEVEL% NEQ 0 (
+        echo ERRO: Falha ao criar o ambiente virtual.
+        EXIT /B %ERRORLEVEL%
+    )
+)
+
 echo Ativando ambiente virtual...
 call venv\Scripts\activate.bat
 IF %ERRORLEVEL% NEQ 0 (
@@ -45,7 +55,7 @@ IF %ERRORLEVEL% NEQ 0 (
     EXIT /B %ERRORLEVEL%
 )
 
-REM 7. Rodar a análise do SonarCloud (ADICIONAR/AJUSTAR ESTA LINHA)
+REM 7. Rodar a análise do SonarCloud
 echo Rodando analise SonarCloud...
 C:\Users\Adriele\Desktop\sonar-scanner-7.1.0.4889-windows-x64\bin\sonar-scanner.bat
 IF %ERRORLEVEL% NEQ 0 (
